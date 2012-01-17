5b984e46a7ebe49f0d052f23ef601caf956e3298
mryoshio <yoshiokaas@gmail.com>
2012-01-18 01:31:05 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Puzzuule::Application.config.secret_token = '870ae1cea8db916b5a68d52aaed7613573ddf2e9ab49c202e80793c74843cb82a15b336da0b77238a606cd7043b45615dbaa8dd10eb3d91a645d023a9b2e6c44'
